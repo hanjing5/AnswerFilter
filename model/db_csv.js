e4b6cbd5c39dbbb9b47b0873ed3a7a8d6d09d428
hjing <hjing@walmartlabs.com>
2013-12-23 15:00:59 -0800

    some minor changes

--- model/db_csv.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var answerSchema = new mongoose.Schema({
-  uid: Number,
-  answer: String
-});
-mongoose.model('Answer', answerSchema);
-
-mongoose.connect('mongodb://localhost/test');
